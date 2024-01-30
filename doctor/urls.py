2c78be51646177dc2aa14fe2a77b658dda3f2f12
Jithin Jose <007jithinjose@gmail.com>
2024-01-30 15:09:44 +0530

    minor commit6

--- doctor/urls.py
+++ doctor/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import doctor_list_create_view,login_api_view
 
 urlpatterns = [
-    path('doctors/', doctor_list_create_view, name='doctor-list-create'),
+    path('signup/', doctor_list_create_view, name='doctor-list-create'),
     path('login/', login_api_view, name='api-login'),
 ]
